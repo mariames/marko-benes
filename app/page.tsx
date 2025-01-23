663149db44c98f209118138446d164f94be7d6f0
marijanadev <marijanavujovic92@gmail.com>
2025-01-23 02:01:23 +0100

    Footer has been started

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import Footer from "@/components/Footer";
 import Logos from "@/components/Logos";
 import Services from "@/components/Services";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start lg:items-center mx-auto">
-      <Services />
-      <Logos />
-      </main>
-    </div>
+    <>
+      <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start lg:items-center mx-auto">
+        <Services />
+        <Logos />
+        
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 }
