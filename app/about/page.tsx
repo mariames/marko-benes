14eb27c19d56236656a2562964308e59246f01f7
marijanadev <marijanavujovic92@gmail.com>
2025-02-07 23:21:41 +0100

    About page has been added

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function About() {
+    return (
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h1>Welcome to My Next.js Project!</h1>
+        <p>This is a simple page example.</p>
+      </div>
+    );
+  }
